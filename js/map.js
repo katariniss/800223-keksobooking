b0d4fe5188d2c615e4518b7d0d762711cd8a00e4
Katariniss <echekina@gmail.com>
2018-11-25 14:00:32 +0500

    Создает массив элементов обьявлений о недвижимости

---
+++ js/map.js
@@ -0,0 +1,226 @@
+var advertisements = [
+  {
+    "author": {
+      "avatar": "img/avatars/user01.png"
+    },
+
+    "offer": {
+      "title": "Большая уютная квартира",
+      "address": "15, 160",
+      "price": 100000,
+      "type":  "flat",
+      "rooms": 3,
+      "guests": 6,
+      "checkin": "12:00",
+      "checkout": "14:00",
+      "features": ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"],
+      "description": "",
+      "photos": ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"],
+    },
+
+    "location": {
+      "x": 15,
+      "y": 160,
+    }
+  },
+
+  {
+    "author": {
+      "avatar": "img/avatars/user02.png"
+    },
+
+    "offer": {
+      "title": "Маленькая неуютная квартира",
+      "address": "40, 250",
+      "price": 30000,
+      "type":  "flat",
+      "rooms": 1,
+      "guests": 2,
+      "checkin": "12:00",
+      "checkout": "14:00",
+      "features": ["wifi", "parking", "elevator", "conditioner"],
+      "description": "",
+      "photos": ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"],
+    },
+
+    "location": {
+      "x": 40,
+      "y": 250,
+    }
+  },
+
+  {
+    "author": {
+      "avatar": "img/avatars/user03.png"
+    },
+
+    "offer": {
+      "title": "Огромный прекрасный дворец",
+      "address": "70, 300",
+      "price": 1000000,
+      "type":  "palace",
+      "rooms": 30,
+      "guests": 30,
+      "checkin": "13:00",
+      "checkout": "13:00",
+      "features": ["wifi", "dishwasher", "parking", "washer", "conditioner"],
+      "description": "",
+      "photos": ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"],
+    },
+
+    "location": {
+      "x": 70,
+      "y": 300,
+    }
+  },
+
+  {
+    "author": {
+      "avatar": "img/avatars/user04.png"
+    },
+
+    "offer": {
+      "title": "Маленький ужасный дворец",
+      "address": "30, 400",
+      "price": 500000,
+      "type":  "palace",
+      "rooms": 10,
+      "guests": 10,
+      "checkin": "14:00",
+      "checkout": "12:00",
+      "features": ["wifi", "parking", "washer"],
+      "description": "",
+      "photos": ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"],
+    },
+
+    "location": {
+      "x": 30,
+      "y": 400,
+    }
+  },
+
+  {
+    "author": {
+      "avatar": "img/avatars/user05.png"
+    },
+
+    "offer": {
+      "title": "Красивый гостевой домик",
+      "address": "80, 450",
+      "price": 89000,
+      "type":  "house",
+      "rooms": 4,
+      "guests": 10,
+      "checkin": "14:00",
+      "checkout": "12:00",
+      "features": ["wifi", "dishwasher", "parking", "washer", "conditioner"],
+      "description": "",
+      "photos": ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"],
+    },
+
+    "location": {
+      "x": 80,
+      "y": 450,
+    }
+  },
+
+  {
+    "author": {
+      "avatar": "img/avatars/user06.png"
+    },
+
+    "offer": {
+      "title": "Некрасивый негостеприимный домик",
+      "address": "100, 50",
+      "price": 30000,
+      "type":  "house",
+      "rooms": 2,
+      "guests": 2,
+      "checkin": "14:00",
+      "checkout": "12:00",
+      "features": ["dishwasher", "parking", "washer", "conditioner"],
+      "description": "",
+      "photos": ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"],
+    },
+
+    "location": {
+      "x": 100,
+      "y": 50,
+    }
+  },
+
+  {
+    "author": {
+      "avatar": "img/avatars/user07.png"
+    },
+
+    "offer": {
+      "title": "Уютное бунгало далеко от моря",
+      "address": "200, 200",
+      "price": 65000,
+      "type":  "bungalo",
+      "rooms": 3,
+      "guests": 6,
+      "checkin": "13:00",
+      "checkout": "12:00",
+      "features": ["wifi", "dishwasher", "parking", "washer", "conditioner"],
+      "description": "",
+      "photos": ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"],
+    },
+
+    "location": {
+      "x": 200,
+      "y": 200,
+    }
+  },
+
+  {
+    "author": {
+      "avatar": "img/avatars/user06.png"
+    },
+
+    "offer": {
+      "title": "Некрасивый негостеприимный домик",
+      "address": "100, 50",
+      "price": 30000,
+      "type":  "house",
+      "rooms": 2,
+      "guests": 2,
+      "checkin": "14:00",
+      "checkout": "12:00",
+      "features": ["dishwasher", "parking", "washer", "conditioner"],
+      "description": "",
+      "photos": ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"],
+    },
+
+    "location": {
+      "x": 100,
+      "y": 50,
+    }
+  },
+
+  {
+    "author": {
+      "avatar": "img/avatars/user08.png"
+    },
+
+    "offer": {
+      "title": "Неуютное бунгало по колено в воде",
+      "address": "250, 120",
+      "price": 40000,
+      "type":  "bungalo",
+      "rooms": 3,
+      "guests": 6,
+      "checkin": "14:00",
+      "checkout": "12:00",
+      "features": ["wifi", "parking"],
+      "description": "",
+      "photos": ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"],
+    },
+
+    "location": {
+      "x": 250,
+      "y": 120,
+    }
+  },
+];
