7ad1412a6e01db68eed20ff5598c527bed9f5927
Katariniss <echekina@gmail.com>
2018-12-05 13:57:14 +0500

    Заканчивает генерировать объект массива (адрес, цена, заголовок, количество гостей, заезд, выезд)

--- js/map.js
+++ js/map.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var HOUSE_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var HOUSE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var ROOMS_MIN = 1;
 var ROOMS_MAX = 5;
@@ -7,7 +8,13 @@ var X_MAX = 980;
 var Y_MAX = 750;
 var AVATAR_MIN = 1;
 var AVATAR_MAX = 8;
+var PRICE_MIN = 1000;
+var PRICE_MAX = 1000000;
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var CHECKIN = ['12:00', '13:00', '14:00'];
+var CHECKOUT = ['12:00', '13:00', '14:00'];
+var GUESTS_MIN = 1;
+var GUESTS_MAX = 10;
 
 var ADS_COUNT = 8;
 
@@ -52,9 +59,17 @@ function getAdvertisement() {
   var roomsNumber = randomInteger(ROOMS_MIN, ROOMS_MAX);
   var houseTypeIndex = randomInteger(0, HOUSE_TYPES.length - 1);
   var houseType = HOUSE_TYPES[houseTypeIndex];
+  var houseTitleIndex = randomInteger(0, HOUSE_TITLES.length - 1);
+  var houseTitle = HOUSE_TITLES[houseTitleIndex];
   var locationX = randomInteger(0, X_MAX);
   var locationY = randomInteger(200, Y_MAX);
   var avatarNumber = randomInteger(AVATAR_MIN, AVATAR_MAX);
+  var price = randomInteger(PRICE_MIN, PRICE_MAX);
+  var checkinIndex = randomInteger(0, CHECKIN.length - 1);
+  var checkin = CHECKIN[checkinIndex];
+  var checkoutIndex = randomInteger(0, CHECKOUT.length - 1);
+  var checkout = CHECKOUT[checkoutIndex];
+  var guestsNumber = randomInteger(GUESTS_MIN, GUESTS_MAX);
 
   return {
     'author': {
@@ -62,14 +77,14 @@ function getAdvertisement() {
     },
 
     'offer': {
-      'title': 'Большая уютная квартира',
-      'address': '300, 400',
-      'price': 100000,
+      'title': houseTitle,
+      'address': locationX + ', ' + locationY,
+      'price': price,
       'type': houseType,
       'rooms': roomsNumber,
-      'guests': 6,
-      'checkin': '12:00',
-      'checkout': '14:00',
+      'guests': guestsNumber,
+      'checkin': checkin,
+      'checkout': checkout,
       'features': generateFeatures(),
       'description': '',
       'photos': shuffle(['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']),
