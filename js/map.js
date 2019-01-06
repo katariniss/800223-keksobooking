7c9f0f16b5eb0fda6a0cbb2586bfe4188092e12a
Katariniss <echekina@gmail.com>
2019-01-06 21:27:23 +0500

    Выносит модуль, который отвечает за создание пина — метки на карте

--- js/map.js
+++ js/map.js
@@ -31,7 +31,7 @@ function resetMapToDefault() {
 function renderMap() {
   var advertisements = window.generateAds();
 
-  createPinsOnMap(advertisements);
+  window.createPinsOnMap(advertisements);
   map.classList.remove('map--faded');
   window.toggleForms(false);
   mapPinMain.removeEventListener('mouseup', renderMap);
@@ -107,34 +107,3 @@ mapPinMain.addEventListener('mousedown', function (evt) {
   document.addEventListener('mousemove', onMouseMove);
   document.addEventListener('mouseup', onMouseUp);
 });
-
-function createPinsOnMap(objects) {
-  var mapPins = document.querySelector('.map__pins');
-  var pinTemplate = document.querySelector('#pin').content.querySelector('button');
-  var fragment = document.createDocumentFragment();
-
-  for (var i = 0; i < objects.length; i++) {
-    fragment.appendChild(getSinglePin(objects[i]));
-  }
-
-  mapPins.appendChild(fragment);
-
-  function getSinglePin(object) {
-    var pin = pinTemplate.cloneNode(true);
-    var pinImg = pin.querySelector('img');
-
-    pin.setAttribute('style', 'left:' + (object.location.x - 25) + 'px;' + 'top:' + (object.location.y - 70) + 'px;');
-    pinImg.setAttribute('src', object.author.avatar);
-    pinImg.setAttribute('alt', object.offer.title);
-
-    pin.addEventListener('click', function () {
-      var openedCard = document.querySelector('.map__card.popup');
-      if (openedCard) {
-        window.removeElementFromDom(openedCard);
-      }
-      window.showPinCard(object);
-    });
-
-    return pin;
-  }
-}
