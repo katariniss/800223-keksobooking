d62c45818dac33bcf390e17111b688fe66a81c59
Katariniss <echekina@gmail.com>
2019-01-08 14:57:38 +0500

    Рефакторинг - выносит работу с сервером в одну функцию в backend.js

--- js/backend.js
+++ js/backend.js
@@ -8,37 +8,17 @@
   };
 
   function loadAdvertisements(onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000; // 10s
-
-    xhr.open('GET', 'https://js.dump.academy/keksobooking/data');
-    xhr.send();
+    sendRequest('GET', 'https://js.dump.academy/keksobooking/data', onSuccess, onError);
   }
 
   function submitAdvertisement(data, onSuccess, onError) {
+    sendRequest('POST', 'https://js.dump.academy/keksobooking', onSuccess, onError, data);
+  }
+
+  function sendRequest(method, url, onSuccess, onError, data) {
     var xhr = new XMLHttpRequest();
 
     xhr.responseType = 'json';
-
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
         onSuccess(xhr.response);
@@ -56,8 +36,8 @@
     });
 
     xhr.timeout = 10000; // 10s
-
-    xhr.open('POST', 'https://js.dump.academy/keksobooking');
+    xhr.open(method, url);
     xhr.send(data);
   }
+
 })();
